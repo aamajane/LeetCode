7ae561ee30908f5a9462b1361e09c978dd6f449d
aamajane <93654070+aamajane@users.noreply.github.com>
2022-04-12 21:06:32 +0000

    Time: 24 ms (30.35%), Space: 6.4 MB (5.84%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.c
+++ 9-palindrome-number/9-palindrome-number.c
@@ -4,11 +4,11 @@ bool isPalindrome(int x)
 {
     if (x < 0)
         return (false);
-    int len = snprintf(NULL, 0, "%d", x);
-    char* str = calloc(len + 1, sizeof(char));
-    snprintf(str, len + 1, "%d", x);
-    for (int i = 0; i < len; i++, len--)
-        if (str[i] != str[len - 1])
+    int xlen = snprintf(NULL, 0, "%d", x);
+    char* str = calloc(xlen + 1, sizeof(char));
+    snprintf(str, xlen + 1, "%d", x);
+    for (int i = 0; i < xlen; i++, xlen--)
+        if (str[i] != str[xlen - 1])
             return (false);
     return (true);
 }
