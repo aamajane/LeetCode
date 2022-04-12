f7cdf5439e2ffb00b9563902af6a67f3e8f4761d
aamajane <93654070+aamajane@users.noreply.github.com>
2022-04-12 20:27:51 +0000

    Time: 142 ms (48.14%), Space: 6.2 MB (99.51%) - LeetHub

--- 1-two-sum/1-two-sum.c
+++ 1-two-sum/1-two-sum.c
@@ -7,7 +7,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     *returnSize = 2;
     int* ret = calloc(*returnSize, sizeof(int));
-    for (int i = 0; i < numsSize; i++)
+    for (int i = 0; i < numsSize - 1; i++)
     {
         for (int j = i + 1; j < numsSize; j++)
         {
