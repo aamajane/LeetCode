5011bbd7938ace04522214a911d5641029d5f400
aamajane <93654070+aamajane@users.noreply.github.com>
2022-04-14 22:55:35 +0000

    Time: 15 ms (68.64%), Space: 7.8 MB (9.89%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.c
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.c
@@ -2,9 +2,9 @@
 
 int removeDuplicates(int* nums, int numsSize)
 {
-    int cnt = 1;
+    int count = 1;
     for (int i = 0; i + 1 < numsSize; i++)
         if (nums[i + 1] > nums[i])
-            nums[cnt++] = nums[i + 1];
-    return (cnt);
+            nums[count++] = nums[i + 1];
+    return (count);
 }
