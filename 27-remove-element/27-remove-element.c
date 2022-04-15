5cbd5eb91883197f68b5f628712bb433a69c3f62
aamajane <93654070+aamajane@users.noreply.github.com>
2022-04-15 20:32:41 +0000

    Time: 4 ms (54.23%), Space: 6 MB (68.57%) - LeetHub

--- 27-remove-element/27-remove-element.c
+++ 27-remove-element/27-remove-element.c
@@ -2,9 +2,9 @@
 
 int removeElement(int* nums, int numsSize, int val)
 {
-    int count = 0;
+    int cnt = 0;
     for (int i = 0; i < numsSize; i++)
         if (nums[i] != val)
-            nums[count++] = nums[i];
-    return (count);
+            nums[cnt++] = nums[i];
+    return (cnt);
 }
