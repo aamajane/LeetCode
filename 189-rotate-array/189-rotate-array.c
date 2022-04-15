7206f057989f7a2e0e3d7deae6569692262ed584
aamajane <93654070+aamajane@users.noreply.github.com>
2022-04-15 21:40:57 +0000

    Time: 110 ms (87.12%), Space: 22.6 MB (16.94%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.c
@@ -0,0 +1,13 @@
+
+
+void rotate(int* nums, int numsSize, int k)
+{
+    if (k == 0 || numsSize == 1 || k % numsSize == 0)
+        return ;
+    if (k > numsSize)
+        k %= numsSize;
+    int* tmp = calloc(numsSize, sizeof(int));
+    memmove(tmp, nums + (numsSize - k), sizeof(int) * k);
+    memmove(tmp + k, nums, sizeof(int) * (numsSize - k));
+    memmove(nums, tmp, sizeof(int) * numsSize);
+}
